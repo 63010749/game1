230469790917a08ef7f45fa9e569944657bdb7ef
pawida sattayamuk <TangTana@hotmail.com>
2020-10-20 09:38:02 +0700

    Add project files.

---
+++ game1/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace game1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.txtAmmo = new System.Windows.Forms.Label();
+            this.txtScore = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.healthBar = new System.Windows.Forms.ProgressBar();
+            this.GameTimer = new System.Windows.Forms.Timer(this.components);
+            this.player = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtAmmo
+            // 
+            this.txtAmmo.AutoSize = true;
+            this.txtAmmo.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtAmmo.Location = new System.Drawing.Point(8, 15);
+            this.txtAmmo.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.txtAmmo.Name = "txtAmmo";
+            this.txtAmmo.Size = new System.Drawing.Size(93, 24);
+            this.txtAmmo.TabIndex = 0;
+            this.txtAmmo.Text = "Ammo: 0";
+            // 
+            // txtScore
+            // 
+            this.txtScore.AutoSize = true;
+            this.txtScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtScore.Location = new System.Drawing.Point(307, 15);
+            this.txtScore.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.txtScore.Name = "txtScore";
+            this.txtScore.Size = new System.Drawing.Size(71, 24);
+            this.txtScore.TabIndex = 1;
+            this.txtScore.Text = "Kills: 0";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(600, 15);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 24);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Health: ";
+            // 
+            // healthBar
+            // 
+            this.healthBar.BackColor = System.Drawing.SystemColors.Control;
+            this.healthBar.ForeColor = System.Drawing.SystemColors.Control;
+            this.healthBar.Location = new System.Drawing.Point(686, 24);
+            this.healthBar.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.healthBar.Name = "healthBar";
+            this.healthBar.Size = new System.Drawing.Size(209, 15);
+            this.healthBar.TabIndex = 3;
+            this.healthBar.Value = 100;
+            // 
+            // GameTimer
+            // 
+            this.GameTimer.Enabled = true;
+            this.GameTimer.Interval = 20;
+            this.GameTimer.Tick += new System.EventHandler(this.MainTimerEvent);
+            // 
+            // player
+            // 
+            this.player.Image = global::game1.Properties.Resources.up;
+            this.player.Location = new System.Drawing.Point(428, 530);
+            this.player.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.player.Name = "player";
+            this.player.Size = new System.Drawing.Size(71, 100);
+            this.player.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.player.TabIndex = 4;
+            this.player.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(924, 661);
+            this.Controls.Add(this.player);
+            this.Controls.Add(this.healthBar);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtScore);
+            this.Controls.Add(this.txtAmmo);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Show;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Love Shooting";
+            this.TransparencyKey = System.Drawing.Color.Transparent;
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.KeyIsDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.KeyIsUp);
+            ((System.ComponentModel.ISupportInitialize)(this.player)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label txtAmmo;
+        private System.Windows.Forms.Label txtScore;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ProgressBar healthBar;
+        private System.Windows.Forms.PictureBox player;
+        private System.Windows.Forms.Timer GameTimer;
+    }
+}
+
---
+++ game1/Form1.cs
@@ -0,0 +1,323 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace game1
+{
+    public partial class Form1 : Form
+    {
+
+        bool goLeft, goRight, goUp, goDown, gameOver;
+        string facing = "up";
+        int playerHealth = 100;
+        int speed = 10;
+        int ammo = 10;
+        int zombieSpeed = 3;
+        Random randNum = new Random();
+        int score;
+        List<PictureBox> zombiesList = new List<PictureBox>();
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+            RestartGame();
+        }
+
+        private void MainTimerEvent(object sender, EventArgs e)
+        {
+            
+            if(playerHealth > 1)
+            {
+                healthBar.Value = playerHealth;
+               
+            }
+            
+            else
+            {
+                gameOver = true;
+                player.Image = Properties.Resources.dead;
+                GameTimer.Stop();
+            }
+
+            txtAmmo.Text = "Ammo: " + ammo;
+            txtScore.Text = "Kills: " + score;
+
+
+            if (goLeft == true && player.Left > 0)
+            {
+                player.Left -= speed;
+            }
+
+            if(goRight == true && player.Left + player.Width < this.ClientSize.Width)
+            {
+                player.Left += speed;
+            }
+
+            if(goUp == true && player.Top > 40)
+            {
+                player.Top -= speed;
+            }
+
+            if(goDown == true && player.Top + player.Height < this.ClientSize.Height)
+            {
+                player.Top += speed;
+            }
+
+            
+
+            foreach(Control x in this.Controls)
+            {
+                if(x is PictureBox && (string)x.Tag == "ammo")
+                {
+                    if (player.Bounds.IntersectsWith(x.Bounds)) 
+                    {
+                        this.Controls.Remove(x);
+                        ((PictureBox)x).Dispose();
+                        ammo += 5;
+                    }
+                }
+            
+                if(x is PictureBox && (string)x.Tag == "zombie")
+                {
+                    if(player.Bounds.IntersectsWith(x.Bounds))
+                    {
+                        playerHealth -= 1;
+                        
+                    }
+                    if (x.Left > player.Left)
+                    {
+                        x.Left -= zombieSpeed;
+                        ((PictureBox)x).Image = Properties.Resources.zleft;
+                    }
+                    if(x.Left  < player.Left)
+                    {
+                        x.Left += zombieSpeed;
+                        ((PictureBox)x).Image = Properties.Resources.zright;
+                    }
+                    if(x.Top > player.Top)
+                    {
+                        x.Top -= zombieSpeed;
+                        ((PictureBox)x).Image = Properties.Resources.zup;
+                    }
+                    if(x.Top < player.Top)
+                    {
+                        x.Top += zombieSpeed;
+                        ((PictureBox)x).Image = Properties.Resources.zdown;
+                    }
+
+                    foreach(Control j in this.Controls)
+                    {
+                        if(j is PictureBox && (string)j.Tag == "bullet" && x is PictureBox && (string)x.Tag == "zombie" )
+                        {
+                            if(x.Bounds.IntersectsWith(j.Bounds))
+                            {
+                                score++;
+                                if (score % 10 == 0 && score != 0)
+                                {
+                                    DropHp();
+                                }
+                                this.Controls.Remove(j);
+                                ((PictureBox)j).Dispose();
+                                this.Controls.Remove(x);
+                                ((PictureBox)x).Dispose();
+                                zombiesList.Remove((PictureBox)x);
+                                MakeZombies();
+                            }
+                        }
+                    }
+                }
+                if (x is PictureBox && (string)x.Tag == "hp")
+                {
+                    if (player.Bounds.IntersectsWith(x.Bounds))
+                    {
+                        this.Controls.Remove(x);
+                        ((PictureBox)x).Dispose();
+                        if (playerHealth < 90)
+                        {
+                            playerHealth += 10;
+                        }
+                        
+                    }
+                }
+
+            }
+
+
+
+        }
+
+        private void KeyIsDown(object sender, KeyEventArgs e)
+        {
+            if(gameOver == true)
+            {
+                return;
+            }
+
+            if (e.KeyCode == Keys.Left)
+            {
+                goLeft = true;
+                facing = "left";
+                player.Image = Properties.Resources.left;
+            }
+
+            if (e.KeyCode == Keys.Right)
+            {
+                goRight = true;
+                facing = "right";
+                player.Image = Properties.Resources.right;
+            }
+
+            if (e.KeyCode == Keys.Up)
+            {
+                goUp = true;
+                facing = "up";
+                player.Image = Properties.Resources.up;
+            }
+
+            if (e.KeyCode == Keys.Down)
+            {
+                goDown = true;
+                facing = "down";
+                player.Image = Properties.Resources.down;
+            }
+
+        }
+
+        private void KeyIsUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left)
+            {
+                goLeft = false;
+            }
+
+            if (e.KeyCode == Keys.Right)
+            {
+                goRight = false;
+            }
+
+            if (e.KeyCode == Keys.Up)
+            {
+                goUp = false;               
+            }
+
+            if (e.KeyCode == Keys.Down)
+            {
+                goDown = false;
+            }
+
+            if(e.KeyCode == Keys.Space && ammo > 0 && gameOver == false )
+            {
+                ammo--;
+                ShootBullet(facing);
+
+                if (ammo < 1)
+                {
+                    DropAmmo();
+                }
+
+            }
+
+            if (e.KeyCode == Keys.Enter && gameOver == true)
+            {
+                
+                RestartGame();
+            }
+
+        }
+
+        private void ShootBullet(string direction)
+        {
+            Bullet shootBullet = new Bullet();
+            shootBullet.direction = direction;
+            shootBullet.bulletLeft = player.Left + (player.Width / 2);
+            shootBullet.bulletTop = player.Top + (player.Height / 2);
+            shootBullet.MakeBullet(this);
+
+        } 
+
+        private void MakeZombies()
+        {
+            // when this function is called it will make zombies in the game
+
+            PictureBox zombie = new PictureBox(); // create a new picture box called zombie
+            zombie.Tag = "zombie"; // add a tag to it called zombie
+            zombie.Image = Properties.Resources.zdown; // the default picture for the zombie is zdown
+            zombie.Left = randNum.Next(0, 900); // generate a number between 0 and 900 and assignment that to the new zombies left 
+            zombie.Top = randNum.Next(0, 800); // generate a number between 0 and 800 and assignment that to the new zombies top
+            zombie.SizeMode = PictureBoxSizeMode.AutoSize; // set auto size for the new picture box
+            zombiesList.Add(zombie);
+            this.Controls.Add(zombie); // add the picture box to the screen
+            player.BringToFront(); // bring the player to the front
+        }
+
+        private void DropHp()
+        {
+            PictureBox hp = new PictureBox(); // create a new instance of the picture box
+            hp.Image = Properties.Resources.hp_Image; // assignment the ammo image to the picture box
+            hp.SizeMode = PictureBoxSizeMode.AutoSize; // set the size to auto size
+            hp.Left = randNum.Next(10, this.ClientSize.Width - hp.Width); // set the location to a random left
+            hp.Top = randNum.Next(40, this.ClientSize.Height - hp.Height); // set the location to a random top
+            hp.Tag = "hp"; // set the tag to ammo
+            this.Controls.Add(hp); // add the ammo picture box to the screen
+            
+            hp.BringToFront(); // bring it to front
+            player.BringToFront(); // bring the player t
+        }
+
+        private void DropAmmo()
+        {
+            // this function will make a ammo image for this game
+
+            PictureBox ammo = new PictureBox(); // create a new instance of the picture box
+            ammo.Image = Properties.Resources.ammo_Image; // assignment the ammo image to the picture box
+            ammo.SizeMode = PictureBoxSizeMode.AutoSize; // set the size to auto size
+            ammo.Left = randNum.Next(10, this.ClientSize.Width - ammo.Width); // set the location to a random left
+            ammo.Top = randNum.Next(40, this.ClientSize.Height - ammo.Height); // set the location to a random top
+            ammo.Tag = "ammo"; // set the tag to ammo
+            this.Controls.Add(ammo); // add the ammo picture box to the screen
+            
+            ammo.BringToFront(); // bring it to front
+            player.BringToFront(); // bring the player to front
+        }
+
+        private void RestartGame()
+        {
+           
+            player.Image = Properties.Resources.up;
+
+            foreach (PictureBox i in zombiesList)
+            {
+                this.Controls.Remove(i);
+            }
+
+             zombiesList.Clear();
+
+
+
+            for (int i = 0; i < 3; i++)
+            {
+                MakeZombies();
+            }
+
+            goUp = false;
+            goDown = false;
+            goLeft = false;
+            goRight = false;
+            gameOver = false;
+
+            playerHealth = 100;
+            score = 0;
+            ammo = 10;
+
+            GameTimer.Start();
+
+        }
+    }
+}
